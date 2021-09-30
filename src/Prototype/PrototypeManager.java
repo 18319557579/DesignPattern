1bf8d6703713a8356e3945dc4694257827205d3e
huangshaofei <1152594956@qq.com>
2021-09-30 13:35:57 +0800

    第一次提交，不完整

---
+++ src/Prototype/PrototypeManager.java
@@ -0,0 +1,10 @@
+package Prototype;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PrototypeManager {
+    private Map<String, Shape> shapeHolder = new HashMap<>();
+
+
+}
---
+++ src/Prototype/Shape.java
@@ -0,0 +1,8 @@
+package Prototype;
+
+public interface Shape extends Cloneable {
+
+    public Object clone();  //拷贝
+
+    public void countArea();  //计算面积
+}
